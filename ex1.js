3ae33be06e734b386cfc26e568fbb9f8115d2c96
shacharmalka1 <shacharmalka0204@gmail.com>
2021-08-30 22:24:30 +0300

    Median function

--- ex1.js
+++ ex1.js
@@ -13,5 +13,18 @@ function Range(arr) {
   return max - min;
 }
 
-const grades = [73, 91, 88, 56, 62, 84, 95, 96, 88, 92];
-document.write(Range(grades));
+function Median(arr) {
+  arr.sort();
+  if (arr.length % 2 == 0) {
+    var average;
+    average = (arr[arr.length / 2] + arr[arr.length / 2 - 1]) / 2;
+    return average;
+  } else {
+    return arr[Math.floor(arr.length / 2)];
+  }
+}
+
+const grades = [73, 91, 88, 56, 62, 84, 95, 96, 88, 92]; //56,62,73,84,88,88,91,92,95,96
+//document.write(Range(grades));
+
+document.write(Median(grades));
